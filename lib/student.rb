c0f8eafe051442a3ad6a6ba5f011c892819f13d4
Adam Fudge <coolfudge@gmail.com>
2017-11-11 17:03:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,6 +21,12 @@ class Student
   end
 
   def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
   def self.create
@@ -29,7 +35,17 @@ class Student
   def self.new_from_db
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def update
