1edbff82a8235918f954e04782e6e60dd590f05d
Adam Fudge <coolfudge@gmail.com>
2017-11-11 17:06:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,11 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+  end
+  
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
